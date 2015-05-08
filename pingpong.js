b14013b3ebaf82a35fcd6e3c221d1059663808b6
kylemellander <kylemellander@gmail.com>
2015-05-08 15:40:32 -0700

    Create pingpong.js

---
+++ pingpong.js
@@ -0,0 +1,7 @@
+var number = prompt("Please provide a whole, positive number");
+
+if(number % 2 === 0) {
+    console.log("The first number is even");
+} else {
+    console.log("The first number is odd");
+}
