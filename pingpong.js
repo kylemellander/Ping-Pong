6d0876b0663da1d89be58051889b2d38029aad02
kylemellander <kylemellander@gmail.com>
2015-05-08 15:57:39 -0700

    Update pingpong.js

--- pingpong.js
+++ pingpong.js
@@ -1,16 +1,16 @@
 var number = prompt("Please provide a whole, positive number");
 
 var pingPong = function (x) {
-    if(number % 3 === 0) {
-        if(number % 5 === 0) {
+    if(x % 3 === 0) {
+        if(x % 5 === 0) {
             console.log("ping-pong");
         } else {
             console.log("ping");
         }
-    } else if (number % 5 === 0 ) {
+    } else if (x % 5 === 0 ) {
         console.log("pong");
     } else {
-        console.log(number);
+        console.log(x);
     }
 }
 
